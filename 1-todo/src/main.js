88d1725ed26d0c077578782963b52a6f997ad90a
Vedran Krizic <vedran.krizic@prodyna.com>
2020-02-17 17:16:13 +0100

    1-todo(first class): Basic structure

--- 1-todo/src/main.js
+++ 1-todo/src/main.js
@@ -1,5 +1,14 @@
 const init = function(){
     console.log("%cInitialised!", 'background: #ff0000; color: #bada55; font-size: 2rem');
+
+
+
 }
 
 init();
+
+$( document ).ready(function() {
+    $("input.todo-title").keypress((e) => {
+        console.log(e.target.value);
+    })
+});
