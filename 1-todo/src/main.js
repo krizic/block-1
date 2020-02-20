04801d4ae1b78a124c6aaebd2e4a0b9a8f1b0b34
Aline Treffot <aline.treffot@studiosus.com>
2020-02-20 18:01:38 +0100

    1-todo: todo app using jQuery

--- 1-todo/src/main.js
+++ 1-todo/src/main.js
@@ -1,14 +1,25 @@
 const init = function(){
     console.log("%cInitialised!", 'background: #ff0000; color: #bada55; font-size: 2rem');
-
-
-
 }
 
 init();
 
 $( document ).ready(function() {
+
     $("input.todo-title").keypress((e) => {
-        console.log(e.target.value);
-    })
+        var key = e.which;
+        var value = e.target.value;
+        if(key == 13 && value) { // the enter key code
+            $("aside.todo-items").append("<div><p>"+value+"</p><button>Remove</button></div>");
+            $("input.todo-title").val('');
+
+         
+            return false;  
+        }
+
+    });
+
+    $("aside.todo-items").on('click', 'button', function(events){
+        $(this).parent().remove();
+     });
 });
