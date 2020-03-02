d359aa62261fa00e18a80e8ce584ae0a8cea33c2
htoinh <htoi.nhkum@studiosus.com>
2020-03-02 20:59:44 +0100

    Bootstrap Project, added counter

--- 2-tinder/src/main.js
+++ 2-tinder/src/main.js
@@ -9,6 +9,76 @@ $(document).ready(function() {
         return responce.json();
     })
     .then((data) => {
-        console.log("Actual data", data);
-    })
+      allUsers = data;
+
+    //   data.results.forEach((user) => {
+    //       new ProfileCard($, user);
+    //   });
+
+    new ProfileCard($, data.results[0]); 
+   
+    new ProfileLike($, data.results[0]);
+
 });
+})
+
+class ProfileCard {
+  $;
+  user;
+
+  constructor(jquery, user){
+    this.$ = jquery;
+    this.user = user;
+    this.render();
+
+    console.log("User", this.user);
+  }
+  
+  render(){
+    this.$("div#profile-view").append(
+      ` <div class="card">
+      <div class="card-header">
+      ${this.user.name.title} ${this.user.name.first} ${this.user.name.last} 
+      </div>
+      <img class="card-img-top" src="${this.user.picture.large}" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">
+        ${this.user.login.username}
+        </h5>
+        <p class="card-text">
+        ${this.user.location.city}, ${this.user.location.state}, ${this.user.location.country}
+        </p>
+      </div>
+    </div>`
+    )
+  }
+}
+
+class ProfileLike {
+    $;
+    LIKE_BUTTON = 'button#like';
+    UNLIKE_BUTTON = 'button#unlike';
+
+    constructor(status){
+        this.$ = status;
+        this.likeInit();
+        this.unlikeInit();
+    }
+
+    likeInit() {
+        this.$(this.LIKE_BUTTON).on('click', e => {
+            $('.output').html(function(i, val) {
+                return val*1+1;
+              });  
+        });
+    }
+
+    unlikeInit() {
+        this.$(this.UNLIKE_BUTTON).on('click', e => {
+/*             $('.minus-output').html(function(i, val) {
+                return val*1-1;
+              });  */ 
+        });
+    }
+    
+}
