318ee371be770ddc50af898d4c3e2ba2655ac168
Lajy7839 <lajy7839@gmail.com>
2020-03-02 15:59:04 +0100

    add Fetch

--- 2-tinder/src/main.js
+++ 2-tinder/src/main.js
@@ -1,2 +1,14 @@
 // Waiting for jQuery to initialize
-$(document).ready(function() {});
+$(document).ready(function() {
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+    // https://randomuser.me/
+
+    fetch("https://randomuser.me/api/?results=200")
+    .then((responce) => {
+        return responce.json();
+    })
+    .then((data) => {
+        console.log("Actual data", data);
+    })
+});
