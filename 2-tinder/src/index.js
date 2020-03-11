34cd32f60857d268e45358679a47d05e4f17b44f
Aline Treffot <aline.treffot@studiosus.com>
2020-03-11 14:39:05 +0100

    add boostrap

--- 2-tinder/src/index.js
+++ 2-tinder/src/index.js
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 import $ from "jquery";
 import {ViewController} from "./view-controller";
+
 
 $(document).ready(function() {
   const constrollerView = new ViewController($);
--- 2-tinder/webpack.config.js
+++ 2-tinder/webpack.config.js
@@ -1,3 +1,5 @@
+
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -21,6 +23,14 @@ module: {
             use: {
                 loader: 'babel-loader' // specify the loader
             } 
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.(ttf|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?|(jpg|gif)$/,
+            loader: 'file-loader'
         }
     ]
 }
