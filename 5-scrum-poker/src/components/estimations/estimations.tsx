47aa7dbf15fdbdf326e197616388807acf053acc
Vedran Krizic <vedrankrizic@gmail.com>
2020-04-23 10:19:53 +0200

    changes from last sesstion

--- 5-scrum-poker/src/components/estimations/estimations.tsx
+++ 5-scrum-poker/src/components/estimations/estimations.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import {Tab} from "semantic-ui-react";
+import {Tab, Segment} from "semantic-ui-react";
 
 import {IEstimation} from "../../api/interfaces";
 import VotesTable from "../votes-table/votes-table";
 
 import "./style.scss";
+import EstimationChart from "../estimation-chart/estimation-chart";
 
 export interface IEstimationsProps {
   estimations: {[key: string]: IEstimation};
@@ -37,27 +38,30 @@ export default class Estimations extends React.Component<
           ? 1
           : -1;
       })
-      .map((estimationKeys, index) => {
+      .map((estimationKey, index) => {
         return {
-          menuItem: this.props.estimations[estimationKeys].isActive
-            ? `${this.props.estimations[estimationKeys].name} - Active`
-            : this.props.estimations[estimationKeys].name,
+          menuItem: this.props.estimations[estimationKey].isActive
+            ? `${this.props.estimations[estimationKey].name} - Active`
+            : this.props.estimations[estimationKey].name,
           render: () => (
             <Tab.Pane className="tab-container">
               <VotesTable
                 documentRef={{_rev: this.props.rev, _id: this.props.id}}
-                estimate={this.props.estimations[estimationKeys]}
+                estimate={this.props.estimations[estimationKey]}
               ></VotesTable>
+              { !(this.props.estimations[estimationKey].isActive) && (<EstimationChart
+                estimation={this.props.estimations[estimationKey]}
+              ></EstimationChart>)}
             </Tab.Pane>
           ),
         };
       });
-  }
+  };
 
   public render() {
     return (
       <Tab
-        menu={{ pointing: true, fluid: true, vertical: true }}
+        menu={{pointing: true, fluid: true, vertical: true}}
         panes={this.mapEstimationsToPanes()}
       />
     );
